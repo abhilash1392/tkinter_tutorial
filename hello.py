85533f395d02171eb5fb1187a6fbc003d5a1546b
abhilash1392 <abhilash1392@gmail.com>
2020-09-09 17:58:22 +0530

    Tkinter

---
+++ hello.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root=Tk()
+# Creating the Label Widget
+myLabel=Label(root,text='Hello World!')
+# Packing it on to the screen
+myLabel.pack()
+
+# Creating the Loop
+root.mainloop()
